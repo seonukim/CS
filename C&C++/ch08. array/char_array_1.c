e17d27a928bf01a6aef4b3f41f5bace1529c2994
seonukim <92.seoonooo@gmail.com>
2020-11-21 01:41:37 +0900

    add comment

--- C&C++/ch08. array/char_array_1.c
+++ C&C++/ch08. array/char_array_1.c
@@ -1,8 +1,11 @@
+// C언어에서는 하나의 무자는 '1bytes'만 담는다
+// 닫시 말해 문자열은 사실 문자 + 배열, 즉 문자의 배열이라고 할 수 있다
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
-    char a[20];
-    scanf("%s", &a);
+    char a[20];             // 기본적으로 문자열을 선언할 때는 문자열의 크기보다 배열의 크기가 크도록 선언
+    scanf("%s", &a);        // 문자열을 입력 받거나 출력할 때는 %s라는 형식 지정자를 사용한다
     printf("%s\n", a);
 }
