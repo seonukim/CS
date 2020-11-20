172e844ff7356804ebface4e5be65dc0e4961506
seonukim <92.seoonooo@gmail.com>
2020-11-21 01:42:22 +0900

    add comment

--- C&C++/ch08. array/char_array_2.c
+++ C&C++/ch08. array/char_array_2.c
@@ -1,8 +1,10 @@
+// C언어는 특정한 인덱서의 문자에 바로 접근할 수 있다
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
     char a[20] = "Hello World";
-    a[4] = ',';
+    a[4] = ',';         // 바로 접근해서 수정 가능
     printf("%s", a);
 }
