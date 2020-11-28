0b33c49b53696c55969c70a05dbe0bd2a109f945
seonukim <92.seoonooo@gmail.com>
2020-11-29 02:12:45 +0900

    commit

--- OpenCV_python/ch01/HelloCV.py
+++ OpenCV_python/ch01/HelloCV.py
@@ -1,2 +1,14 @@
+import sys
 import cv2
-print("Hello, OpenCV, ", cv2.__version__)
+print('Hello CV', cv2.__version__)
+
+img = cv2.imread('cat.bmp')
+
+if img is None:
+    print('Image load failed!')
+    sys.exit()
+
+cv2.namedWindow('image')        # image라는 window 생성
+cv2.imshow('image', img)        # image창에 img 를 띄워줌
+cv2.waitKey()                   # 키보드 입력을 기다림
+cv2.destroyAllWindows()         # 창 닫기
