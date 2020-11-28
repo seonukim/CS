fc38dcf8027c6218dbd6742e0305b0a9b8bdea7c
seonukim <92.seoonooo@gmail.com>
2020-11-29 03:14:27 +0900

    commit

--- OpenCV_python/ch01/matplotlib_test.py
+++ OpenCV_python/ch01/matplotlib_test.py
@@ -7,6 +7,6 @@ path = '/Users/seonu/Documents/SoyNet/study/CS/OpenCV_python/ch01/images/'
 imgBGR = cv2.imread(path + '1.jpg')
 imgRGB = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2RGB)        # opencv는 BGR로 나타내서 RGB로 바꿔줘야함
 
-plt.axis('off')         # 가로세로 눈금
+plt.axis('off')         # 가로세로 눈금 안보이게
 plt.imshow(imgRGB)
 plt.show()
